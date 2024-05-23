fba4653579518f68e6ebbc743408233cd72a77bd
brooklinepeacecoalition <brooklinepeacecoalition@gmail.com>
2024-05-23 11:28:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Yes on 19!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Yes on 19!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          1. Select your precinct</code>
         </p>
       </main>
 
